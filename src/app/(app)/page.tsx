a45a01dfe195e4c37ee62ffb914207308e58dab4
manishprasad7173@gmail.com <manishprasad7173@gmail.com>
2025-01-15 22:32:13 +0530

    Enhance homepage layout with backend development focus and login instructions

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -4,8 +4,11 @@ import React from 'react'
 
 function page() {
   return (
-    <div>
-      home
+    <div className='p-8 '>
+      <h1 className='flex flex-wrap justify-center text-4xl font-bold '>This Project is mainly focussed on Backend Development.</h1>
+      <p className='flex flex-wrap justify-center text-3xl'>Try to Log in</p>
+      <p className='flex flex-wrap justify-center text-2xl'>Username: admin</p>
+      <p className='flex flex-wrap justify-center text-2xl'>Password: 111111</p>
     </div>
   )
 }
